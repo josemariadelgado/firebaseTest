da52d99ea724bbb072d82d15afec602cd67d8b06
José María Delgado <jose.d@treexor.com>
2016-07-30 22:25:50 +0100

    First commit

---
+++ firebaseTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  firebaseTest
+//
+//  Created by José María Delgado  on 30/7/16.
+//  Copyright © 2016 José María Delgado. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
